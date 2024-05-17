03638a97c4f2aa511e0a82dd21d65d6d9fc40633
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-05-17 11:31:18 -0400

    feat: init clt fnb site

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cltfnb
+# Charlotte Food Not Bombs
+This is the repository for the Charlotte Food Not Bombs website at [cltfnb.com](cltfnb.com).
---
+++ vocs.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'Charlotte Food Not Bombs',
+  sidebar: [
+    {
+      text: 'Home Page',
+      link: '/home-page',
+    },
+    {
+      text: 'Other FNB Chapters',
+      link: '/other-fnb-chapters',
+    },
+  ],
+})
