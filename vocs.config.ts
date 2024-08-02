28f1d6554dea652d7808059e9ad18307b0465e94
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-08-02 11:55:39 -0400

    feat: add how to guides

--- vocs.config.ts
+++ vocs.config.ts
@@ -11,5 +11,19 @@ export default defineConfig({
       text: 'Other FNB Chapters',
       link: '/other-fnb-chapters',
     },
+    {
+      text: 'How To Guides',
+      collapsed: false,
+      items: [
+        {
+          text: 'How To Set Up An Open Collective',
+          link: '/how-to-guides/how-to-set-up-an-open-collective',
+        },
+        {
+          text: 'How To Make A Site Like This',
+          link: '/how-to-guides/how-to-make-a-site-like-this',
+        }
+      ]
+    }
   ],
 })
